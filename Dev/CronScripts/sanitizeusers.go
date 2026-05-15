ec663e0614fe5bb6494d047a9ec9c57cda5453a5
TheCanadianCodingGuy <duosterne@pm.me>
2026-05-14 20:26:51 -0400

    updated sanitize users to fix max discord api calls.

--- Dev/CronScripts/sanitizeusers.go
+++ Dev/CronScripts/sanitizeusers.go
@@ -1,7 +1,33 @@
-{{/* --- CRON: 0 0 * * * --- */}}
-{{ range (dbTopEntries "TEST_banana_slips" 100 0) }}
+{{/* --- CRON: */30 * * * * --- */}}
+{{ $batchSize := 8 }} {{/* Number of users to check per run */}}
+{{ $ptrKey := "TEST_cleanup_pointer" }}
+{{ $lbKey := "TEST_banana_slips" }}
+
+{{/* 1. Retrieve the current offset pointer */}}
+{{ $offset := 0 }}
+{{ with (dbGet 0 $ptrKey) }}
+    {{ $offset = (toInt .Value) }}
+{{ end }}
+
+{{/* 2. Fetch the next batch of entries using the offset */}}
+{{ $entries := dbTopEntries $lbKey $batchSize $offset }}
+
+{{/* 3. If we hit the end of the list, reset the pointer and exit */}}
+{{ if not $entries }}
+    {{ dbSet 0 $ptrKey 0 }}
+    {{ return }}
+{{ end }}
+
+{{/* 4. Process the batch */}}
+{{ range $entries }}
     {{ if not (getMember .User.ID) }}
+        {{/* Target identified as having left the server: Wipe all related keys */}}
         {{ dbDel .User.ID "TEST_banana_slips" }}
         {{ dbDel .User.ID "TEST_banana_data" }}
+        {{ printf "Successfully purged inactive user: %d" .User.ID }}
     {{ end }}
 {{ end }}
+
+{{/* 5. Update the pointer for the next execution */}}
+{{ $newOffset := add $offset $batchSize }}
+{{ dbSet 0 $ptrKey $newOffset }}
--- Production/CronScripts/sanitizeusers.go
+++ Production/CronScripts/sanitizeusers.go
@@ -1,21 +1,35 @@
-{{/* Daily Cleanup: Slips & Flips (PROD) */}}
+{{/* --- CRON: */30 * * * * --- */}}
+{{ $batchSize := 8 }} {{/* Number of users to check per run */}}
+{{ $ptrKey := "cleanup_pointer" }}
+{{ $lbKey := "banana_slips" }}
 
-{{/* 1. Clean up based on Slips Leaderboard */}}
-{{ range (dbTopEntries "banana_slips" 100 0) }}
+{{/* 1. Retrieve the current offset pointer */}}
+{{ $offset := 0 }}
+{{ with (dbGet 0 $ptrKey) }}
+    {{ $offset = (toInt .Value) }}
+{{ end }}
+
+{{/* 2. Fetch the next batch of entries using the offset */}}
+{{ $entries := dbTopEntries $lbKey $batchSize $offset }}
+
+{{/* 3. If we hit the end of the list, reset the pointer and exit */}}
+{{ if not $entries }}
+    {{ dbSet 0 $ptrKey 0 }}
+    {{ return }}
+{{ end }}
+
+{{/* 4. Process the batch */}}
+{{ range $entries }}
     {{ if not (getMember .User.ID) }}
+        {{/* Target identified as having left the server: Wipe all related keys */}}
         {{ dbDel .User.ID "banana_slips" }}
         {{ dbDel .User.ID "banana_cooldown" }}
         {{ dbDel .User.ID "banana_current_backflip" }}
         {{ dbDel .User.ID "banana_record_backflip" }}
+        {{ printf "Successfully purged inactive user: %d" .User.ID }}
     {{ end }}
 {{ end }}
 
-{{/* 2. Clean up based on Record Backflip Leaderboard */}}
-{{ range (dbTopEntries "banana_record_backflip" 100 0) }}
-    {{ if not (getMember .User.ID) }}
-        {{ dbDel .User.ID "banana_slips" }}
-        {{ dbDel .User.ID "banana_cooldown" }}
-        {{ dbDel .User.ID "banana_current_backflip" }}
-        {{ dbDel .User.ID "banana_record_backflip" }}
-    {{ end }}
-{{ end }}
+{{/* 5. Update the pointer for the next execution */}}
+{{ $newOffset := add $offset $batchSize }}
+{{ dbSet 0 $ptrKey $newOffset }}
