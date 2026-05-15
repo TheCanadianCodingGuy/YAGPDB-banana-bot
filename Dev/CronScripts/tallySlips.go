444767f220da2a24adfd5e5f13d481a3d0ee5d8c
TheCanadianCodingGuy <duosterne@pm.me>
2026-05-15 10:52:19 -0400

    Testing Pinning messages automatically.

--- Dev/CronScripts/tallySlips.go
+++ Dev/CronScripts/tallySlips.go
@@ -1,6 +1,7 @@
 {{/* --- CRON: 35 18 28-31 * * --- */}}
 {{ $now := currentTime }}
 
+{{/* 1. Date Check: Hardcoded for May 15th Test */}}
 {{/* 1. Date Check */}}
 {{ $nextMonth := (newDate $now.Year (add (toInt $now.Month) 1) 1 0 0 0) }}
 {{ if eq (toInt $now.Month) 12 }}{{ $nextMonth = (newDate (add $now.Year 1) 1 1 0 0 0) }}{{ end }}
@@ -11,7 +12,18 @@
 {{ $keySlips := "TEST_banana_slips" }}
 {{ $keyPrestige := "TEST_prestige_global" }}
 {{ $keyGlobal := "TEST_banana_global" }}
+{{ $keyPin := "TEST_season_announcement" }}
+{{ $chanID := 1438831937220378674 }}
 
+{{/* 3. Unpin Management (Remove previous "Season Begun" pin) */}}
+{{ with (dbGet 0 $keyPin) }}
+    {{ $oldMsgID := toInt64 .Value }}
+    {{ if (getMessage (toInt64 $chanID) $oldMsgID) }}
+        {{ unpinMessage (toInt64 $chanID) $oldMsgID }}
+    {{ end }}
+{{ end }}
+
+{{/* 4. Fetch Leaderboard & Global Data */}}
 {{ $entries := dbTopEntries $keySlips 100 0 }}
 {{ $prestigeMap := sdict }}
 {{ with (dbGet 0 $keyPrestige) }}{{ $prestigeMap = dict .Value | sdict }}{{ end }}
@@ -19,11 +31,9 @@
 {{ $global := sdict "season" 1 }}{{ with (dbGet 0 $keyGlobal) }}{{ $global = dict .Value | sdict }}{{ end }}
 {{ $seasonN := toInt ($global.Get "season") }}
 
-{{/* 3. Ranking Logic & Label Mapping */}}
+{{/* 5. Ranking Logic */}}
 {{ $currentRank := 0 }}{{ $rewardedCount := 0 }}{{ $prevScore := -1 }}{{ $hasTies := false }}{{ $winnersList := cslice }}
 {{ $pointsMap := dict 1 5 2 3 3 1 }}
-
-{{/* Maps for Emoji and Text */}}
 {{ $medalMap := dict 1 "🥇" 2 "🥈" 3 "🥉" }}
 {{ $textMap := dict 1 "First" 2 "Second" 3 "Third" }}
 
@@ -43,7 +53,6 @@
         {{ $newTotal := add $oldTotal $points }}
         {{ $prestigeMap.Set (str $uid) $newTotal }}
         
-        {{/* Retrieve Medal and Text */}}
         {{ $medal := index $medalMap $currentRank }}
         {{ $rankText := index $textMap $currentRank }}
         
@@ -55,15 +64,16 @@
     {{ end }}
 {{ end }}
 
-{{/* 4. Save */}}
+{{/* 6. Save Prestige & Send Announcement */}}
 {{ dbSet 0 $keyPrestige $prestigeMap }}
 
-{{/* 5. Format & Send */}}
 {{ $header := "🏆 **THE BANANA POINTS HAVE BEEN TALLIED!** 🏆\n" }}
 {{ $tieNote := "" }}{{ if $hasTies }}{{ $tieNote = "*Note: Ties were detected. Slippers with the same score share the same rank level and prizes.*\n" }}{{ end }}
 {{ $footer := printf "\nCongratulations to our winners of **season %d**.\n\n🛠️ Preparations for **season %d** are underway! The new season will begin at <t:%d:F> (<t:%d:R>)." $seasonN (add $seasonN 1) $nextMonth.Unix $nextMonth.Unix }}
 
 {{ $announcement := printf "%s\n%s\n%s\n%s" $header $tieNote (joinStr "\n" $winnersList) $footer }}
 
-{{ $channelID := 1438831937220378674 }}
-{{ sendMessage (toInt64 $channelID) $announcement }}
+{{/* Send, Pin, and Track the new Tally message */}}
+{{ $newMsgID := sendMessageRetID (toInt64 $chanID) $announcement }}
+{{ pinMessage (toInt64 $chanID) $newMsgID }}
+{{ dbSet 0 $keyPin (str $newMsgID) }}
