7672ed2160c3ed57efb2e4dc0ac30f7ad17c70df
TheCanadianCodingGuy <duosterne@pm.me>
2026-05-31 15:43:41 -0400

    With removing multiple prods and all that, moving to a pure branching solution and we have a real production now.

--- NewProduction/1- Initiation Scripts/3- -oldseasonpurge.go
+++
@@ -1,9 +0,0 @@
-{{$entries := dbTopEntries "banana_slips" 1 0}}
-
-{{if $entries}}
-    {{$target := index $entries 0}}
-    {{dbDel $target.UserID "banana_slips"}}
-    {{dbDel $target.UserID "banana_record_backflip"}}
-    {{dbDel $target.UserID "banana_current_backflip"}}
-    {{dbDel $target.UserID "banana_cooldown"}}
-{{end}}
